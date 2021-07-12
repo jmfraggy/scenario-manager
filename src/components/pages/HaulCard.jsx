71538014c59842009f274616b0c7dcfc4018b73a
jmfraggy <jmfofragoso@gmail.com>
2021-07-12 12:40:09 -0500

    [ADD] UI/UX styles

--- src/components/pages/HaulCard.jsx
+++ src/components/pages/HaulCard.jsx
@@ -1,15 +1,37 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import LocalShippingIcon from '@material-ui/icons/LocalShipping';
 import CircularProgressWithLabel from './CircularProgressWithLabel';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import Divider from '@material-ui/core/Divider';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(even)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const tableData = [
+  { id: 1, name: 'PPP', ammount: 3 },
+  { id: 2, name: 'POC', ammount: 2 },
+  { id: 3, name: 'OBT', ammount: 1 },
+]
 
 const HaulCard = ({ title, expand }) => {
   const [swapIcon, setSwapIcon] = useState(true);
 
-  useEffect(()=> {
+  useEffect(() => {
     return () => {
-        setSwapIcon(expand);
+      setSwapIcon(expand);
     };
   }, [expand]);
 
@@ -18,31 +40,60 @@ const HaulCard = ({ title, expand }) => {
   }
 
   return (
-    <div className={`haul-card ${swapIcon ? '' : 'card-resize'}`}>
-      <div className="haul-title">
-        <LocalShippingIcon />
-        <h3>{title}</h3>
-        <h4>06/12</h4>
-        <CircularProgressWithLabel
-          variant="determinate"
-          value={50}
-          type="card"
-        />
-      </div>
+    <Paper elevation={3} className={`haul-card ${swapIcon ? '' : 'card-resize'}`}>
+      <Grid container>
+        <Grid container item xs={12} className="haul-title">
+          <Grid item xs={1}>
+            <LocalShippingIcon />
+          </Grid>
+          <Grid item xs={5} className="haul-title-description">
+            <h3>{title}</h3>
+            <p>251 - 750 miles</p>
+          </Grid>
+          <Grid item xs={5} className="haul-load">
+            <div>
+              <h4>06</h4>
+              <p>Demand</p>
+            </div>
+            <Divider className="divider" orientation="vertical" flexItem />
+            <div>
+              <h4>12</h4>
+              <p>Capacity</p>
+            </div>
+          </Grid>
+          <Grid item xs={1} className="progress">
+            <CircularProgressWithLabel
+              variant="determinate"
+              value={50}
+              type="card"
+            />
+          </Grid>
+        </Grid>
 
-      <div className="segcodes-btn">
-        <button onClick={onCollapseSegment}>
-          <h4>Segment Codes</h4>
-          {swapIcon ? <ExpandLessIcon />: <ExpandMoreIcon />}
-        </button>
-      </div>
-      {swapIcon ? <div className={`segcodes-table ${swapIcon ? 'visible' : ''}`} >
-        <p>PPP</p>
-        <p>03</p>
-        <p>PPP</p>
-        <p>03</p>
-      </div> : null}
-    </div>
+        <Grid item xs={12} className="segcodes-btn">
+          <button onClick={onCollapseSegment}>
+            <h4>Segment Codes</h4>
+            {swapIcon ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </button>
+        </Grid>
+
+        {swapIcon ? (
+          <Grid item xs={12} className="segcodes-table-container">
+            <Table className={`segcodes-table ${swapIcon ? 'visible' : ''}`}>
+              <TableBody>
+                {tableData.map((data) => (
+                  <StyledTableRow key={data.id}>
+                    <TableCell style={{ borderBottom: "none" }}>
+                      {data.name}
+                    </TableCell>
+                    <TableCell align="right" style={{ borderBottom: "none" }}>{data.ammount}</TableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Grid>) : null}
+      </Grid>
+    </Paper >
   );
 };
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,7 +5,10 @@ import HaulCard from './HaulCard';
 // Material UI
 import ClearIcon from '@material-ui/icons/Clear';
 import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
 import CircularProgressWithLabel from './CircularProgressWithLabel';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 
 const Home = ({ swapModal = 1 }) => {
   const [visible, setVisible] = useState(swapModal ? swapModal : 0);
@@ -23,29 +26,59 @@ const Home = ({ swapModal = 1 }) => {
   return (
     <div className={`modal-container ${visible ? 'visible' : ''}`}>
       <div className="po-modal">
-        <div className="capacity-title">
-          <h2>Capacity / Demand Region</h2>
-          <button onClick={onCollapseAll} className="button button-2 button-2--orange">
-            {collapseAll ? 'Expand All' : 'Collapse All'}
-          </button>
-          <IconButton onClick={onCloseModal}>
-            <ClearIcon />
-          </IconButton>
-        </div>
-        <div className="capacity-cards">
-          {arr.map((el) => {
-            return <HaulCard key={el} title={el} expand={collapseAll} />;
-          })}
-        </div>
-        <div className="capacity-total">
-          <h3 className="capacity-total-title">Total:</h3>
-          <h3>24/48</h3>
-          <CircularProgressWithLabel
-            variant="determinate"
-            value={50}
-            type="total"
-          />
-        </div>
+        <Grid container>
+
+          <Grid container item xs={12} className="capacity-title" style={{ margin: '0.8em' }}>
+            <Grid item xs={8} md={9} className="capacity-title-text">
+              <h2>SW40</h2>
+            </Grid>
+            <Grid item xs={2} md={2} className="capacity-title-button">
+              <Button onClick={onCollapseAll} variant="outlined" color="inherit" fullWidth className="collapse-button">
+                {collapseAll ? 'Expand All' : 'Collapse All'}
+              </Button>
+            </Grid>
+            <Grid item xs={2} md={1} className="po-modal-x">
+              <IconButton onClick={onCloseModal}>
+                <ClearIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+
+          <Grid container item xs={12} className="capacity-cards" style={{ margin: '0 0.5em' }}>
+            {arr.map((el) => {
+              return (<Grid item md={12} lg={6}>
+                <HaulCard key={el} title={el} expand={collapseAll} />
+              </Grid>);
+            })}
+          </Grid>
+
+          <Grid container item xs={12} className="capacity-total" style={{ margin: '1em' }}>
+            <Grid item xs={4} md={6}>
+              <h3 className="capacity-total-title">Total:</h3>
+            </Grid>
+            <Grid item xs={6} md={5}>
+              <div className="haul-load">
+                <div>
+                  <h4>24</h4>
+                  <p>Demand</p>
+                </div>
+                <Divider className="divider divider-white" orientation="vertical" flexItem />
+                <div>
+                  <h4>48</h4>
+                  <p>Capacity</p>
+                </div>
+              </div>
+            </Grid>
+            <Grid item xs={2} md={1} className="progress">
+              <CircularProgressWithLabel
+                variant="determinate"
+                value={50}
+                type="total"
+              />
+            </Grid>
+          </Grid>
+
+        </Grid>
       </div>
     </div>
   );
